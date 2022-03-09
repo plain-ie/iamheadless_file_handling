c12083865d696473da2360a87d13b040b3a05c86
Maris Erts <7067057+mariserts@users.noreply.github.com>
2022-03-09 10:24:02 +0200

    Update utils.py

--- iamheadless_file_handling/utils.py
+++ iamheadless_file_handling/utils.py
@@ -1,6 +1,18 @@
+import uuid
+
 from .conf import settings
 from .loader import load
 
 
 def get_file_handling_backend():
     return load(settings.FILE_HANDLING_BACKEND_CLASS)
+
+
+def get_file_name(prefix, file_name):
+    if prefix.startswith('/') is False:
+        prefix = '/' + prefix
+    if prefix.endswith('/') is False:
+        prefix = prefix + '/'
+    id = str(uuid.uuid4()).split('-')[-1]
+    file_name = f'{id}-{file_name}'
+    return f'{prefix}{file_name}'
